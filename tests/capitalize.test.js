2e957a885ea37d2f2ed959c42b4b30f9434f171f
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-01-28 16:23:01 +0300

    testing with assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,21 @@
 import { capitalize } from '../src/capitalize.js';
+import { strict as assert } from 'node:assert';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-console.log('Все тесты пройдены!');
+// console.log('Все тесты пройдены!');
+
+// assert(capitalize('') === '');
+// assert(capitalize('hello') === 'Hello');
+
+// assert.equal(capitalize(''), '');
+// assert.equal(capitalize('hello'), 'Hello');
+
+assert.strictEqual(capitalize(''), '');
+assert.strictEqual(capitalize('hello'), 'Hello');
