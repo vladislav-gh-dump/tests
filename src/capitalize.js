fdf3638aff5b136ac82b5b17b04e9f03859c856b
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-01-28 13:31:27 +0300

    test capitalize

---
+++ src/capitalize.js
@@ -0,0 +1,8 @@
+export const capitalize = (text) => {
+  if (text === '') {
+    return '';
+  }
+  const firstChar = text[0].toUpperCase();
+  const restSubstring = text.slice(1);
+  return `${firstChar}${restSubstring}`;
+};
