ab4d503f43306aa1dac121d06d71e6558b857178
vladislav-gh-dump <vladislav.notebook@gmail.com>
2024-01-29 23:14:33 +0300

    testing users with jest

--- __test__/users.test.js
+++ __test__/users.test.js
@@ -3,16 +3,25 @@ import { users } from '../src/users.js';
 
 test('users.length', () => {
   expect(users).toHaveLength(3);
+  expect(users).not.toHaveLength(2);
 });
 
 test('user.name', () => {
   expect(users[0].name).toEqual('Kate');
+  expect(users[1].name).not.toEqual('Name');
 });
 
 test('user.hasFriends', () => {
   expect(users[1].hasFriends).toBeTruthy();
+  expect(users[2].hasFriends).not.toBeTruthy();
+  expect(users[2].hasFriends).toBeFalsy();
 });
 
 test('user.email', () => {
   expect(users[1].email).toBeNull();
+  expect(users[2].email).not.toBeNull();
+});
+
+test('users property', () => {
+  expect(users[0]).toHaveProperty('name', 'Kate');
 });
--- src/users.js
+++ src/users.js
@@ -1,5 +1,5 @@
 export const users = [
   { name: 'Kate', email: 'kate123@gamil.com', hasFriends: true },
   { name: 'Mark', email: null, hasFriends: true },
-  { name: 'Bob', email: 'booob@gamil.com', hasFriends: true },
+  { name: 'Bob', email: 'booob@gamil.com', hasFriends: false },
 ];
